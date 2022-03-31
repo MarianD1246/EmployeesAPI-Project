381240ee5ffdc59b9e486ce95c81da8db687be04
MarianD1246 <marian.dumitriu15@gmail.com>
2022-03-31 19:11:49 +0100

    Created SetUp and initial test for services

--- EmployeesAPI/EmployeeAPITesting/EmployeeControllerTests.cs
+++ EmployeesAPI/EmployeeAPITesting/EmployeeControllerTests.cs
@@ -8,37 +8,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Moq;
+using EmployeesAPI.Model;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeAPITesting
 {
     public class EmployeeControllerTests
     {
 
-        private NorthwindContext? _context;
-        private EmployeeService? _service;
-        private EmployeesController? _sut;
-
+        private Mock<NorthwindContext>? _context;
+        private EmployeesController _sut;
         [OneTimeSetUp]
         public void OneTimeSetup()
         {
-            var options = new DbContextOptionsBuilder<NorthwindContext>().UseInMemoryDatabase(databaseName: "ExampleDb").Options;
-            _context = new NorthwindContext(options);
-            _service = new EmployeeService(_context); //Service needs to be a Mock
-            _sut = new EmployeesController(_context);
-            foreach (Employee emp in TestEmpolyeesDataSource.EmployeesList)
-            {
-                _service.CreateItemAsync(emp);
-            }
+            _context = new Mock<NorthwindContext>();
+            _context.Setup(e => e.Employees.AddRange(TestEmpolyeesDataSource.EmployeesList.ToArray()));
+            _sut = new EmployeesController(_context.Object);
         }
 
         [Test]
-        public void GivenAValidId_GetTodoItemById_ReturnsCorrectItem()
+        public void EmpolyeesControllerSuccessfulConstruction()
         {
+            Assert.That(_sut, Is.InstanceOf<EmployeesController>());
+        }
+
+        [Test]
+        public void GetById_GivenAValidId_ReturnsEmpolyee()
+        {
+            var getEmployeeById = _sut.GetById(1);
+            Assert.That(getEmployeeById.IsCompleted);
+            Assert.That(getEmployeeById.Result.Value, Is.InstanceOf<IEnumerable<EmployeeDTO>>());
+            //var emp = getEmployeeById.Result.Value.ToList();
+            //Assert.That(emp[0], Is.EqualTo(TestEmpolyeesDataSource.EmployeesList[0]));
+            
             //var result = _sut.GetEmployeeById(0).Result;
             //Assert.That(result, Is.Not.Null);
             //Assert.That(result.Value, Is.TypeOf<Employee>());
             //Assert.That(result.Value.FirstName, Is.EqualTo("Bob"));
         }
-
+        [Test]
+        public void PutEmployee_GivenAValidName_ReturnsEmpolyee()
+        {
+            //var Getemployeename = _sut.GetByName();
+            //Assert.That(Getemployeename.IsCompleted);
+        }
     }
 }
